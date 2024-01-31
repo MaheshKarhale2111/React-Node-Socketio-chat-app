add1df6f38593b0329559722575826f13ad8cd5a
MaheshKarhale2111 <maheshkarhale03@gmail.com>
2024-01-31 13:17:11 +0530

    changed styling

--- src/components/JoinFolder/Join.jsx
+++ src/components/JoinFolder/Join.jsx
@@ -8,13 +8,13 @@ export default function Join() {
   return (
     <div className="joinOuterContainer">
       <div className="joinInnerContainer">
-        <h1 className="heading">Join</h1>
+        <h1 className="heading">Welcome to Chat App</h1>
         <div>
           <input type="text" placeholder="Name" className="joinInput" onChange={(event)=> setName(event.target.value)} />
           <input type="text" placeholder="Room" className="joinInput mt-20" onChange={(event)=> setRoom(event.target.value)} />
 
           <Link onClick={event =>(!name || !room ? event.preventDefault() : null)} to={`/chat?name=${name}&room=${room}`}>
-          <button className="button mt-20" type="submit">Sign In</button>
+          <button className="button mt-20" type="submit">Join Chat!</button>
           </Link>
         </div>
       </div>
--- src/components/TextContainer/TextContainer.jsx
+++ src/components/TextContainer/TextContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import onlineIcon from "../icons/onlineIcon.png";
+import { Link } from "react-router-dom";
 
 import "./textcontainer.css";
 
@@ -9,29 +10,24 @@ export default function TextContainer({ users }) {
 
   return (
     <div className="textContainer">
-      <div>
+      <div className="header-div" >
         <h1>
           Realtime Chat Application{" "}
           <span role="img" aria-label="emoji">
             💬
           </span>
         </h1>
-        <h2>
-          Created with React, Express, Node and Socket.IO{" "}
-          <span role="img" aria-label="emoji">
-            ❤️
-          </span>
-        </h2>
-        <h2>
-          Try it out right now!{" "}
-          <span role="img" aria-label="emoji">
-            ⬅️
-          </span>
-        </h2>
+        <h2>Made with React, Express, Node and Socket.IO </h2>
+        
+      </div>
+
+      <div className="help">
+          <h2>Open another session with same 
+            room name and join the chat!!</h2>
       </div>
       {users ? (
-        <div>
-          <h1>People currently chatting:</h1>
+        <div className="current-users">
+          <h1>People Currently Online:</h1>
           <div className="activeContainer">
             <h2>
               {users.map(({ name }) => (
